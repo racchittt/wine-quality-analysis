22d3b94c971ad7123b1411264a0618955f03372d
racchittt <112688112+racchittt@users.noreply.github.com>
2023-10-24 22:49:40 +0530

    Formatted the frontend and added Requirements.txt fike

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import pickle 
 import numpy as np
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/')
